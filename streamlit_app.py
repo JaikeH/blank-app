b3814d81bb7165ef404f4dbeec576af6a3135c6b
jaike2 <jaike2@gmail.com>
2024-11-01 12:23:59 +0000

    asd

--- streamlit_app.py
+++ streamlit_app.py
@@ -188,8 +188,9 @@ def render_dashboard(df, metric):
             gb = GridOptionsBuilder.from_dataframe(related_opps)
             gb.configure_default_column(editable=False, sortable=True, filter=True, wrapText=True, autoHeight=True, width=75)
             gridOptions = gb.build()
-            with st.container():
-            AgGrid(related_opps, gridOptions=gridOptions, height=400, allow_unsafe_jscode=True, fit_columns_on_grid_load=True)
+           with st.container():
+       AgGrid(related_opps, gridOptions=gridOptions, height=400, allow_unsafe_jscode=True, fit_columns_on_grid_load=True)
+
             st.markdown(
                 """
                 <style>
